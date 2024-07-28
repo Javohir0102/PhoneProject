3c433731f2c894f22327ec25a3c09ae6d84ff047
Javohir0102 <javohiranvarov96@gmail.com>
2024-07-28 05:34:54 +0500

    Add Spectre.Console Librery

--- PhoneProject/Program.cs
+++ PhoneProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using PhoneProject.Models;
 using PhoneProject.Services;
+using Spectre.Console;
 
 namespace PhoneProject
 {
@@ -10,12 +11,49 @@ namespace PhoneProject
         static void Main(string[] args)
         {
             ContactServices contactServices = new ContactServices();
-            contactServices.ExistedContacts();
-            contactServices.ReadAllContacts();
-            contactServices.AddContact();
-            contactServices.RemoveContact();
-            contactServices.EditContact();
-            contactServices.ReadAllContacts();
+
+            // Ask for the user's favorite fruit
+            var fruit = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("What's your [green]favorite fruit[/]?")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .AddChoices(new[] {
+            "AddContact", "RemoveContact", "EditContact",
+            "ExistedContacts", "ReadAllContacts",
+                    }));
+
+            // Echo the fruit back to the terminal
+            AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+
+            switch (fruit)
+            {
+                case "AddContact":
+                    {
+                        contactServices.AddContact();
+                        break;
+                    }
+                case "RemoveContact":
+                    {
+                        contactServices.RemoveContact();
+                        break;
+                    }
+                case "EditContact":
+                    {
+                        contactServices.EditContact();
+                        break;
+                    }
+                case "ExistedContacts":
+                    {
+                        contactServices.ExistedContacts();
+                        break;
+                    }
+                case "ReadAllContacts":
+                    {
+                        contactServices.ReadAllContacts();
+                        break;
+                    }
+            }
         }
     }
 }
